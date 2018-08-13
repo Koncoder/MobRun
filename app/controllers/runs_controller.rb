3161143060c9d5086d0e6dc9622272d172215bc8
Konstantin Baumer <baumer.konstantin@gmail.com>
2018-08-13 17:06:49 -0400

    navbar not yet finished

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -1,4 +1,7 @@
 class RunsController < ApplicationController
+
+  skip_before_action :authenticate_user!, only: :index
+
   def index
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+  root to: "runs#index"
+
   resources :users
   resources :sessions
   resources :runs
