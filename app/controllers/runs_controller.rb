30e1050e08596cac2a7e97a1581875ac30b219cd
Sawyer Gilker <sawyer.gilker@hotmail.com>
2018-08-13 17:05:33 -0400

    ...

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -1,14 +1,25 @@
 class RunsController < ApplicationController
   def index
+    @runs = Run.all
   end
 
   def show
+    @run = Run.find(params[:id])
   end
 
   def new
+    @run = Run.new
   end
 
   def create
+    @run = Run.new(
+      name: run_params["name"]
+      )
+    if @run.save
+      redirect_to @run
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,5 +29,14 @@ class RunsController < ApplicationController
   end
 
   def destroy
+    Run.destroy(params[:id])
+    redirect_to runs_path
   end
+
+  private
+
+  def run_params
+    params.require(:run).permit(:name)
+  end
+
 end
