d56f5e0a0944ca59820fea4e3c46af07c312f62f
sara <sarneijim@gmail.com>
2018-08-15 16:35:11 -0400

    withous js

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,9 +1,1 @@
 
-var myMap = document.getElementsByTagName('iframe')[0];
-myMap.addEventListener("load", function() {
-  console.log("cargada");
-  var graphic = document.getElementById('elevation-profile');
-  console.log($("iframe").contents().find("#elevation-profile"))
-  $("iframe").contents().find("#elevation-profile").style.border = '1px solid #000000'
-  console.log("despues");
-});
