16b7e94cd5f7d4da9182ef99cd105911346d105d
Sawyer Gilker <sawyer.gilker@hotmail.com>
2018-08-15 12:22:15 -0400

    header fade

---
+++ app/javascript/packs/headerfade.js
@@ -0,0 +1,13 @@
+
+const fade = () => {
+  var myElement = $('.header');
+
+  $(window).on('scroll', function() {
+      var st = $(this).scrollTop();
+      myElement.css({
+          'opacity' : 1 - st/600
+      });
+  });
+
+}
+export default fade;
