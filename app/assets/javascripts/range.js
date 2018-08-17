2f3e528a74833eb0bb2941b1e47799b266f5b903
sara <sarneijim@gmail.com>
2018-08-17 09:02:59 -0400

    hola

--- app/assets/javascripts/range.js
+++ app/assets/javascripts/range.js
@@ -6,26 +6,26 @@ total = parseFloat($(".number-range-word").html());
 $( "#uislider" ).slider({
   range: "max",
   min: 0,
-  max: total,
+  max: total*10,
   value: 0,
   slide: function( event, ui ) {
-  $( "#run_session_start_point" ).val( ui.value );
+  $( "#run_session_start_point" ).val( ui.value / 10 );
     console.log(ui);
     //sliderValue = ui.value;
-    if( ui.value >= 0 && ui.value <= total/3 ) {
+    if( ui.value >= 0 && ui.value <= total*10/3 ) {
       console.log("green");
         $('.color').css("background-color" , 'green');
         $('.ui-slider-handle').css("background-color" , 'green');
         $('.run_session_start_point input').css("background-color" , 'green');
 
     }
-    if(ui.value > total/3 && ui.value <= 2*total/3 ) {
+    if(ui.value > total*10/3 && ui.value <= 2*total*10/3 ) {
       console.log("orange");
         $('.color').css("background-color" , 'orange');
         $('.ui-slider-handle').css("background-color" , 'orange');
         $('.run_session_start_point input').css("background-color" , 'orange');
     }
-    if( ui.value > 2*total/3 && ui.value <= total ) {
+    if( ui.value > 2*total*10/3 && ui.value <= total*10 ) {
       console.log("red");
         $('.color').css("background-color" , 'red')
         $('.ui-slider-handle').css("background-color" , 'red')
