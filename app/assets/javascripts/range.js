7cd9c69f925abb3fd84d9f20d660514c8554ef76
sara <sarneijim@gmail.com>
2018-08-17 12:06:24 -0400

    new session

--- app/assets/javascripts/range.js
+++ app/assets/javascripts/range.js
@@ -7,33 +7,30 @@ $( "#uislider" ).slider({
   range: "max",
   min: 0,
   max: total*10,
-  value: 0,
+  value: total*10,
   slide: function( event, ui ) {
-  $( "#run_session_start_point" ).val( ui.value / 10 );
-    console.log(ui);
+    $( "#run_session_start_point" ).val( Math.round((total - ui.value / 10) * 100) / 100);
+    $( "#run_session_length" ).html(ui.value / 10 );
     //sliderValue = ui.value;
     if( ui.value >= 0 && ui.value <= total*10/3 ) {
-      console.log("green");
-        $('.color').css("background-color" , 'green');
-        $('.ui-slider-handle').css("background-color" , 'green');
-        $('.run_session_start_point input').css("background-color" , 'green');
+      $('.color').css("background-color" , 'red')
+      $('.ui-slider-handle').css("background-color" , 'red')
 
     }
     if(ui.value > total*10/3 && ui.value <= 2*total*10/3 ) {
-      console.log("orange");
-        $('.color').css("background-color" , 'orange');
-        $('.ui-slider-handle').css("background-color" , 'orange');
-        $('.run_session_start_point input').css("background-color" , 'orange');
+        $('.color').css("background-color" , "#fa8231");
+        $('.ui-slider-handle').css("background-color" , "#fa8231");
+
     }
     if( ui.value > 2*total*10/3 && ui.value <= total*10 ) {
       console.log("red");
-        $('.color').css("background-color" , 'red')
-        $('.ui-slider-handle').css("background-color" , 'red')
-        $('.run_session_start_point input').css("background-color" , 'red');
+      $('.color').css("background-color" , '#20bf6b');
+      $('.ui-slider-handle').css("background-color" , '#20bf6b');
+
+
     }
   }
 });
-
-$( "#run_session_start_point" ).val($( "#uislider").slider( "value" ));
-
+$( "#run_session_start_point" ).val( 0);
+$( "#run_session_length" ).html($( "#uislider").slider( "value" )/10);
 
